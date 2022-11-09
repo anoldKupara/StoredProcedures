11396100e52291d3db523b22a50b992d905b975d
Anold Kupara <kuparaanold20@gmail.com>
2022-11-09 14:50:39 +0200

    Registered services inside the Program class

--- StoredProcedures/Program.cs
+++ StoredProcedures/Program.cs
@@ -1,7 +1,11 @@
+using StoredProcedures.Data;
+using StoredProcedures.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddDbContext<DbContextClass>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
